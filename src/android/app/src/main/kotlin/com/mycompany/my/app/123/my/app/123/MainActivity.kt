e34b5c6120a00019eaef6e3fdcb23a68355a473e
stevewalturn <steve@walturn.com>
2024-12-27 12:56:19 +0700

    Update files (batch 1/1)

---
+++ src/android/app/src/main/kotlin/com/mycompany/my/app/123/my/app/123/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.my_app_123.my_app_123
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ src/lib/main/main_development.dart
@@ -0,0 +1,10 @@
+import 'package:my_app_123/main/bootstrap.dart';
+import 'package:my_app_123/models/enums/flavor.dart';
+import 'package:my_app_123/ui/views/app/app_view.dart';
+
+void main() {
+  bootstrap(
+    builder: () => const AppView(),
+    flavor: Flavor.development,
+  );
+}
